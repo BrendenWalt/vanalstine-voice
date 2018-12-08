dc26ef8fae258d495c0b807f4a0664a036ac9a96
Brenden W <brendenwalters42@gmail.com>
2018-12-08 18:47:22 -0500

    Styled the menu list, and added a JS toggle function to show/hide.

--- app/js/scripts.js
+++ app/js/scripts.js
@@ -1,9 +1,20 @@
 document.addEventListener("DOMContentLoaded", function(e) {
     console.log('JS is loaded');
 
-    $('.nav-burger').click(function(){
-		$(this).toggleClass('open');
-	});
+    var menuIcon = $('.nav-burger');
+    var menuList = $('.nav-links')
+
+    // $('.nav-burger').click(function(){
+    // $(this).toggleClass('open');
+
+    var onMenuClick = function() {
+      console.log('menu-clicked');
+      $(this).toggleClass('open');
+      menuList.toggleClass('open')
+    }
+    
+    menuIcon.on('click', onMenuClick);
+
 });
 
 
